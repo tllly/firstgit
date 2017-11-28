af0604ae9b72d99c5d626cf06aec6cca7180409e
tangli <tllly11@163.com>
2017-11-28 17:53:34 +0800

    js修改

--- src/js/action.js
+++ src/js/action.js
@@ -1,6 +1,7 @@
 module.exports = {
 	'sayHello':function(){
-		console.log('hello world')
+		// console.log('hello world')
+		document.getElementById('mydiv').innerHTML = 'hello world'
 	},
 	'jump':function(){
 		console.log('jump')
